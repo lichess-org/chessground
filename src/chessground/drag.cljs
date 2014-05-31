9202df34fc6c9037a2203645fc53070c2703763a
Thibault Duplessis <thibault.duplessis@gmail.com>
2014-06-01 00:30:46 +0200

    improve dragging by shifting the piece under the cursor

--- src/chessground/drag.cljs
+++ src/chessground/drag.cljs
@@ -6,7 +6,18 @@
 
 (def over-class "drag-over")
 
-(defn- drag-move [draggie event pointer]
+(defn- on-start [_ event _]
+  "Shift piece right under the cursor"
+  (let [$el ($ (.-target event))
+        pos (.offset $el)
+        center-x (+ (.-left pos) (/ (.width $el) 2))
+        center-y (+ (.-top pos) (/ (.height $el) 2))
+        decay-x (- (.-pageX event) center-x)
+        decay-y (- (.-pageY event) center-y)]
+    (jq/css $el {:left (str decay-x "px") :top (str decay-y "px")})))
+
+(defn- on-move [_ event _]
+  "Highlight the square under the dragged piece"
   (let [$el ($ (common/square-element (.-target event)))]
     (when (not (jq/has-class $el over-class))
       (jq/remove-class (jq/siblings $el) over-class)
@@ -14,12 +25,15 @@
 
 (defn make [channels component]
   "Make a react component draggable"
-  (q/wrapper component :onMount (fn [node] (-> (new js/Draggabilly node)
-                                               (.on "dragStart" (push-args! (:drag-start channels)))
-                                               (.on "dragMove" drag-move)
-                                               (.on "dragEnd" (push-args! (:drag-end channels)))))))
+  (q/wrapper component
+             :onMount (fn [node]
+                        (-> (new js/Draggabilly node)
+                            (.on "dragStart" on-start)
+                            (.on "dragStart" (push-args! (:drag-start channels)))
+                            (.on "dragMove" on-move)
+                            (.on "dragEnd" (push-args! (:drag-end channels)))))))
 
-(defn end [[draggie event pointer]]
+(defn end [[draggie event _]]
   "Undo damages done by dragging and return origin and destination square keys"
   (jq/css ($ (.-element draggie)) {:top 0 :left 0})
   (when-let [orig (square-key (.-element draggie))]
