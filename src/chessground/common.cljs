4369050bba80cb1650da23d2fc94a260bb0b1fb8
Thibault Duplessis <thibault.duplessis@gmail.com>
2014-05-31 11:23:52 +0200

    improve drag move over square effect

--- src/chessground/common.cljs
+++ src/chessground/common.cljs
@@ -13,8 +13,14 @@
 
 (defn push-args! [chan] (fn [& args] (push! chan args)))
 
+(defn has-class [dom-element class] (.contains (.-classList dom-element) class))
+
+(defn square-element [dom-element]
+  "If element is a square, return it. If it's a piece, return its parent"
+  (if (has-class dom-element "square")
+    dom-element
+    (when (has-class dom-element "piece") (square-element (.-parentNode dom-element)))))
+
 (defn square-key [dom-element]
   "Gets the square key from the element, or its parent"
-  (keyword (or
-             (.getAttribute dom-element "data-key")
-             (.getAttribute (.-parentNode dom-element) "data-key"))))
+  (keyword (.getAttribute (square-element dom-element) "data-key")))
--- src/chessground/drag.cljs
+++ src/chessground/drag.cljs
@@ -4,9 +4,18 @@
             [jayq.core :as jq :refer [$]]
             [chessground.common :as common :refer [pp square-key push-args!]]))
 
+(def over-class "drag-over")
+
+(defn- drag-move [draggie event pointer]
+  (let [$el ($ (common/square-element (.-target event)))]
+    (when (not (jq/has-class $el over-class))
+      (jq/remove-class (jq/siblings $el) over-class)
+      (jq/add-class $el over-class))))
+
 (defn make [component on-end]
   "Make a react component draggable"
   (q/wrapper component :onMount (fn [node] (-> (new js/Draggabilly node)
+                                               (.on "dragMove" drag-move)
                                                (.on "dragEnd" on-end)))))
 
 (defn end [[draggie event pointer]]
