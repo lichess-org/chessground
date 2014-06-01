7ff623a9435fa9c3afaeaf91cee8bd5d66aaa941
Thibault Duplessis <thibault.duplessis@gmail.com>
2014-06-01 11:05:01 +0200

    remove ring dependency

--- project.clj
+++ project.clj
@@ -1,14 +1,11 @@
 (defproject chessground "0.1.0"
-  :plugins [[lein-cljsbuild "1.0.3"]
-            [lein-ring "0.8.10"]]
+  :plugins [[lein-cljsbuild "1.0.3"]]
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/clojurescript "0.0-2227"]
                  [org.clojure/core.async "0.1.303.0-886421-alpha"]
                  [quiescent-thib/quiescent-thib "0.1.2-THIB"]
                  [com.facebook/react "0.9.0.2"]
-                 [jayq "2.5.1"]
-                 [ring "1.2.1"]]
-  :ring {:handler chessground.serve/handler}
+                 [jayq "2.5.1"]]
   :cljsbuild {:builds
               {:dev {:source-paths ["src"]
                      :compiler
