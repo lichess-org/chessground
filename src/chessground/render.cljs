50baf0305dc0d1cd4d870041cf469a3f79a97e4b
Thibault Duplessis <thibault.duplessis@gmail.com>
2014-06-05 14:37:31 +0200

    optimize repaint by narrowing the state passed to sub components

--- src/chessground/render.cljs
+++ src/chessground/render.cljs
@@ -14,32 +14,37 @@
 (q/defcomponent Piece
   "A piece in a square"
   [[{color :color role :role} targets] channels]
+  ; (pp "render piece")
   (let [piece (d/div {:className (class-name #{"piece" (name color) (name role)})})]
     (if targets (drag/make channels piece targets) piece)))
 
 (q/defcomponent Square
   "One of the 64 board squares"
-  [state key channels]
+  [{selected :selected movable :movable chess :chess} key channels]
+  ; (pp (str "render square " key))
   (let [attributes {:className (class-name #{"square"
-                                             (when (= (:selected state) key) "selected")})
+                                             (when selected "selected")})
                     :key (name key) ; react.js key just in case it helps performance
                     :data-key (name key)}
-        targets (if (-> state :movable :free) :all (-> state :movable :valid key))
+        targets (if (:free movable) :all (-> movable :valid key))
         behaviors (when targets {:onClick #(push! (:select-square channels) key)
                                  :onTouchStart (fn [event]
                                                  (.preventDefault event)
                                                  (push! (:select-square channels) key))})]
     (d/div (merge attributes behaviors)
-           (when-let [piece (chess/get-piece (:chess state) key)]
+           (when-let [piece (chess/get-piece chess key)]
              (Piece [piece targets] channels)))))
 
 (q/defcomponent Board
   "The whole board"
   [state channels]
+  ; (pp "render board")
   (let [white (= (:orientation state) :white)
         squares (for [rank (if white (range 8 0 -1) (range 1 9))
                       file (seq (if white "abcdefgh" "hgfedcba"))]
-                  (Square state (keyword (str file rank)) channels))]
+                  (Square {:selected (= (:selected state) key)
+                           :movable (:movable state)
+                           :chess (:chess state)} (keyword (str file rank)) channels))]
     (apply d/div {:className "board"} squares)))
 
 (q/defcomponent App
