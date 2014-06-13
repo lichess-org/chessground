7fe6e02609287be5d56bf84a96305c1c2a7c1476
Thibault Duplessis <thibault.duplessis@gmail.com>
2014-06-13 11:53:16 +0200

    remove react and dependant implementations

--- project.clj
+++ project.clj
@@ -3,8 +3,6 @@
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/clojurescript "0.0-2227"]
                  [org.clojure/core.async "0.1.303.0-886421-alpha"]
-                 [quiescent-thib/quiescent-thib "0.1.2-THIB"]
-                 [com.facebook/react "0.9.0.2"]
                  [jayq "2.5.1"]]
   :cljsbuild {:builds
               {:dev {:source-paths ["src"]
@@ -18,8 +16,7 @@
                       :compiler
                       {:output-to "generated/chessground.min.js"
                        :optimizations :advanced
-                       :preamble ["react/react.min.js"]
-                       :externs ["react/externs/react.js" "externs/misc.js" "externs/jquery.js"]
+                       :externs ["externs/misc.js" "externs/jquery.js"]
                        :pretty-print true
                        :closure-warnings {:non-standard-jsdoc :off}}}}})
 
--- src/chessground.cljs
+++ src/chessground.cljs
@@ -1,10 +1,6 @@
 (ns chessground
   (:require [cljs.core.async :as a]
             [chessground.common :as common :refer [pp]]
-            [chessground.api :as api]
-            [chessground.render :as render]
-            [chessground.data :as data]
-            [chessground.ctrl :as ctrl]
             [clojure.walk :refer [keywordize-keys]])
   (:require-macros [cljs.core.async.macros :as am]))
 
@@ -12,7 +8,7 @@
   "Return a map containing the initial application"
   [dom-element config]
   {:dom-element dom-element
-   :state (atom (data/make config))
+   :state (atom {})
    :render-pending? (atom false)
    :channels {:toggle-orientation (a/chan)
               :set-orientation (a/chan)
@@ -24,36 +20,9 @@
               :move-start (a/chan)
               :move-piece (a/chan)
               }
-   :consumers {:toggle-orientation ctrl/toggle-orientation
-               :set-orientation ctrl/set-orientation
-               :set-fen ctrl/set-fen
-               :set-dests ctrl/set-dests
-               :set-color ctrl/set-color
-               :clear ctrl/clear
-               :select-square ctrl/select-square
-               :move-start ctrl/move-start
-               :move-piece ctrl/move-piece
-               }
    })
-
-(defn init-updates
-  "For every entry in a map of channel identifiers to consumers,
-   initiate a channel listener which will update the application state
-   using the appropriate function whenever a value is recieved, as
-   well as triggering a render."
-  [app]
-  (doseq [[ch update-fn] (:consumers app)]
-    (am/go (while true
-             (let [val (a/<! (get (:channels app) ch))
-                   _ (pp (str "on channel [" ch "], received value [" val "]"))
-                   new-state (swap! (:state app) update-fn val)]
-               (render/request-render app))))))
 
 (defn ^:export main
   "Application entry point; returns the public JavaScript API"
   [dom-element config]
-  (let [app (load-app dom-element (or (keywordize-keys (js->clj config)) {}))]
-    (when common/is-touch-device (.initializeTouchEvents js/React true))
-    (init-updates app)
-    (render/request-render app)
-    (api/build (:channels app))))
+  (pp config))
