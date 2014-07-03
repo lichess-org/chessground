2372ef01098f09c756a43141a8487821da97a1fd
Vincent Velociter <vincent.velociter@gmail.com>
2014-07-03 16:37:47 +0200

    Fix render coordinates

--- src/chessground/render.cljs
+++ src/chessground/render.cljs
@@ -6,10 +6,15 @@
 (defn piece [{color :color role :role}]
   (str "<div class='piece " color " " role "'></div>"))
 
-(defn square [key pos p]
+(defn square [key pos p white]
   (let [style (apply str (map (fn [[k v]] (str (name k) ":" v ";")) pos))
-        coord-y (if (= (subs key 0 1) "a") (str "data-coord-y='" (subs key 1 2) "' ") "")
-        coord-x (if (= (subs key 1 2) "1") (str "data-coord-x='" (subs key 0 1) "' ") "")
+        coord-y (if white
+                  (if (= (subs key 0 1) "a") (str "data-coord-y='" (subs key 1 2) "' ") "")
+                  (if (= (subs key 0 1) "h") (str "data-coord-y='" (subs key 1 2) "' ") ""))
+
+        coord-x (if white
+                  (if (= (subs key 1 2) "1") (str "data-coord-x='" (subs key 0 1) "' ") "")
+                  (if (= (subs key 1 2) "8") (str "data-coord-x='" (subs key 0 1) "' ") ""))
         piece-html (when p (piece p))]
     (str "<div class='square' " coord-y coord-x "data-key='" key "' style='" style "'>" piece-html "</div>")))
 
@@ -23,7 +28,7 @@
                             pos {(if white :left :right) (str (* (- file-n 1) 12.5) "%")
                                  (if white :bottom :top) (str (* (- rank 1) 12.5) "%")}
                             p (chess/get-piece c key)]]
-                  (square key pos p))]
+                  (square key pos p white))]
     (str "<div class='board'>" (apply str squares) "</div>")))
 
 (defn spare-pieces [state color klass]
