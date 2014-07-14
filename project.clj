7cfa00f5f97e92fd564d59158be0898584645304
Thibault Duplessis <thibault.duplessis@gmail.com>
2014-07-14 11:19:00 +0200

    improve build file

--- project.clj
+++ project.clj
@@ -1,4 +1,7 @@
-(defproject chessground "0.1.0"
+(defproject org.lichess/chessground "0.1.0"
+  :description "Extendable basics for chess UIs."
+  :license {:name "MIT"
+            :url "http://opensource.org/licenses/MIT"}
   :plugins [[lein-cljsbuild "1.0.3"]]
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/clojurescript "0.0-2268"]
