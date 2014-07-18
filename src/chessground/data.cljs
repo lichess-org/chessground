1855f05e1707f3d07a0154ee55607e4682932ccc
Olli Piepponen <opieppo@gmail.com>
2014-07-18 22:46:54 +0700

    Schematized data.cljs

--- src/chessground/data.cljs
+++ src/chessground/data.cljs
@@ -1,9 +1,12 @@
 (ns chessground.data
   "Representation and manipulation of the application data"
   (:require [chessground.common :as common :refer [pp]]
-            [chessground.chess :as chess]))
+            [chessground.chess :as chess]
+            [chessground.schemas :refer [ChessState]]
+            [schema.core :as s :refer [Str Bool]])
+  (:require-macros [schema.macros :as sm :refer [defschema]]))
 
-(def defaults
+(sm/def defaults :- ChessState
   "Default state, overridable by user configuration"
   {:fen nil ; replaced by :chess by data/make
    :chess {} ; representation of a chess game
@@ -21,9 +24,14 @@
    :spare-pieces false ; provide extra pieces to put on the board)
    })
 
-(defn with-fen [state fen] (assoc state :chess (chess/make fen)))
 
-(defn make [js-config]
+(sm/defn with-fen :- ChessState
+  [state :- ChessState
+   fen :- Str]
+  (assoc state :chess (chess/make fen)))
+
+(sm/defn make :- ChessState
+  [js-config :- js/Object]
   (let [config (-> js-config
                    common/keywordize-keys
                    (common/keywordize-keys-in [:movable])
@@ -32,26 +40,34 @@
         (with-fen (:fen config))
         (dissoc :fen))))
 
-(defn clear [state]
+(sm/defn clear :- ChessState
+  [state :- ChessState]
   (-> state
       (assoc :chess chess/clear)
       (assoc-in [:movable :dests] nil)))
 
-(defn is-movable? [state key]
+(sm/defn is-movable? :- Bool
   "Piece on this square may be moved somewhere, if the validation allows it"
+  [state :- ChessState
+   key :- Str]
   (let [owner (chess/owner-color (:chess state) key)
         movable (:movable state)
         color (:color movable)]
     (and owner (or (= color "both") (= color owner)))))
 
-(defn can-move? [state orig dest]
+(sm/defn can-move? :- Bool
   "The piece on orig can definitely be moved to dest"
+  [state :- ChessState
+   orig :- Str
+   dest :- Str]
   (and (is-movable? state orig)
        (or (-> state :movable :free)
            (common/set-contains? (get-in state [:movable :dests orig]) dest))))
 
-(defn dests-of [state orig]
+(sm/defn dests-of :- (s/maybe [Square])
   "List of destinations square keys for this origin"
+  [state :- ChessState
+   orig :- Str]
   (when (is-movable? state orig)
     (if (-> state :movable :free)
       (for [rank (range 1 9)
