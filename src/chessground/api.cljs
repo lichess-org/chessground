2d43eacb8ad87bc49290a13830e3cd641ec0f452
Thibault Duplessis <thibault.duplessis@gmail.com>
2014-08-28 20:53:15 +0200

    abstract over get API

--- src/chessground/api.cljs
+++ src/chessground/api.cljs
@@ -8,31 +8,28 @@
 (defn build
   "Creates JavaScript functions that push to the channel"
   [chan]
-  (letfn [(push [function data] (common/push! chan [function data]))]
+  (letfn [(tell [function data] (common/push! chan [function data]))
+          (ask [question callback]
+            (let [response-chan (a/chan)]
+              (am/go (a/>! chan [question response-chan])
+                     (callback (clj->js (a/<! response-chan)))
+                     (a/close! response-chan))))]
     (clj->js
-      {:toggleOrientation #(push :toggle-orientation nil)
-       :setOrientation    #(push :set-orientation %)
-       :getOrientation    (fn [callback]
-                            (let [response-chan (a/chan)]
-                              (am/go (a/>! chan [:get-orientation response-chan])
-                                     (callback (a/<! response-chan))
-                                     (a/close! response-chan))))
-       :getPosition       (fn [callback]
-                            (let [response-chan (a/chan)]
-                              (am/go (a/>! chan [:get-position response-chan])
-                                     (callback (clj->js (a/<! response-chan)))
-                                     (a/close! response-chan))))
-       :setFen            #(push :set-fen %)
-       :setStartPos       #(push :set-fen "start")
-       :clear             #(push :clear nil)
-       :move              #(push :api-move [%1 %2])
-       :setLastMove       #(push :set-last-move [%1 %2])
-       :setCheck          #(push :set-check %)
+      {:toggleOrientation #(tell :toggle-orientation nil)
+       :setOrientation    #(tell :set-orientation %)
+       :getOrientation    #(ask :get-orientation %)
+       :getPosition       #(ask :get-position %)
+       :setFen            #(tell :set-fen %)
+       :setStartPos       #(tell :set-fen "start")
+       :clear             #(tell :clear nil)
+       :move              #(tell :api-move [%1 %2])
+       :setLastMove       #(tell :set-last-move [%1 %2])
+       :setCheck          #(tell :set-check %)
        :setPieces         (fn [pieces]
-                            (push :set-pieces (common/map-values
+                            (tell :set-pieces (common/map-values
                                                 common/keywordize-keys
                                                 (js->clj pieces {:keywordize-keys true}))))
-       :setDests          #(push :set-dests (js->clj %))})))
+       :setDests          #(tell :set-dests (js->clj %))})))
 ; "setFen"            (fn [fen] (push-in :set-fen fen))
 ; "startPos"          (fn [] (push-in :set-fen "start"))
 ; "setDests"          (fn [dests] (push-in :set-dests (js->clj dests)))
