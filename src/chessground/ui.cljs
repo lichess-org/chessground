ae593413819ec598c6ceb879790f176988f992a6
Thibault Duplessis <thibault.duplessis@gmail.com>
2014-09-09 00:33:16 +0200

    generate all keys with loops

--- src/chessground/ui.cljs
+++ src/chessground/ui.cljs
@@ -121,14 +121,13 @@
                                  square-component))))}))
 
 (def ^private all-keys
-  #js ["a1" "a2" "a3" "a4" "a5" "a6" "a7" "a8"
-       "b1" "b2" "b3" "b4" "b5" "b6" "b7" "b8"
-       "c1" "c2" "c3" "c4" "c5" "c6" "c7" "c8"
-       "d1" "d2" "d3" "d4" "d5" "d6" "d7" "d8"
-       "e1" "e2" "e3" "e4" "e5" "e6" "e7" "e8"
-       "f1" "f2" "f3" "f4" "f5" "f6" "f7" "f8"
-       "g1" "g2" "g3" "g4" "g5" "g6" "g7" "g8"
-       "h1" "h2" "h3" "h4" "h5" "h6" "h7" "h8"])
+  (let [arr (array)]
+    (loop [rank 1]
+      (loop [file 1]
+        (.push arr (str (aget "abcdefgh" (dec file)) rank))
+        (when (< file 8) (recur (inc file))))
+      (when (< rank 8) (recur (inc rank))))
+    arr))
 
 (defn- array-of [coll] (if coll (clj->js coll) (array)))
 
