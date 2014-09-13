a49bdfc894a5c3a0ec4df8b59f0e41c2de3cc268
Vincent Velociter <vincent.velociter@gmail.com>
2014-09-13 12:35:34 +0200

    Bump 0.3.6

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.lichess/chessground "0.3.5"
+(defproject org.lichess/chessground "0.3.6"
   :description "Extendable basics for chess UIs."
   :license {:name "MIT" :url "http://opensource.org/licenses/MIT"}
   :plugins [[lein-cljsbuild "1.0.3"]]
