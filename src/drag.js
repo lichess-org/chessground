ecbaef632dd8cdd2c41391595590a28caf0fdc1d
Thibault Duplessis <thibault.duplessis@gmail.com>
2014-09-30 11:01:35 +0200

    move drag logic and repaint to RAF

--- src/drag.js
+++ src/drag.js
@@ -16,6 +16,7 @@ function start(ctrl, e) {
     ctrl.data.draggable.current = {
       orig: orig,
       rel: position,
+      epos: position,
       pos: [0, 0],
       dec: ctrl.data.draggable.centerPiece ? [
         position[0] - (pieceBounds.left + pieceBounds.width / 2),
@@ -25,28 +26,32 @@ function start(ctrl, e) {
       started: false
     };
   }
-  ctrl.data.render();
+  var processDrag = function() {
+    requestAnimationFrame(function() {
+      var cur = ctrl.data.draggable.current;
+      if (cur.orig) {
+        // cancel animations while dragging
+        if (ctrl.data.animation.current.start) ctrl.data.animation.current = {};
+        if (!cur.started && util.distance(cur.epos, cur.rel) > ctrl.data.draggable.distance)
+          cur.started = true;
+        if (cur.started) {
+          cur.pos = [
+            cur.epos[0] - cur.rel[0],
+            cur.epos[1] - cur.rel[1]
+          ];
+          cur.over = board.getKeyAtDomPos(ctrl.data, cur.epos, cur.bounds);
+        }
+      }
+      ctrl.data.render();
+      if (cur.orig) processDrag();
+    });
+  };
+  renderDrag();
 }
 
 function move(ctrl, e) {
-  var cur = ctrl.data.draggable.current;
-  if (!cur.orig) return;
-
-  var position = util.eventPosition(e);
-
-  if (!cur.started && util.distance(position, cur.rel) > ctrl.data.draggable.distance)
-    cur.started = true;
-
-  if (cur.started) {
-    // cancel animations while dragging
-    if (ctrl.data.animation.current.start) ctrl.data.animation.current = {};
-    cur.pos = [
-      position[0] - cur.rel[0],
-      position[1] - cur.rel[1]
-    ];
-    cur.over = board.getKeyAtDomPos(ctrl.data, position, cur.bounds);
-  }
-  ctrl.data.render();
+  if (ctrl.data.draggable.current.orig)
+    ctrl.data.draggable.current.epos = util.eventPosition(e);
 }
 
 function end(ctrl, e) {
@@ -59,7 +64,6 @@ function end(ctrl, e) {
     board.userMove(ctrl.data, orig, dest);
   }
   draggable.current = {};
-  ctrl.data.render();
 }
 
 module.exports = {
