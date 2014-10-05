1c48f478bc6db0b3b7cdac4ac6337f6f00ac610a
Thibault Duplessis <thibault.duplessis@gmail.com>
2014-10-05 10:20:02 +0200

    fix up the api

--- src/api.js
+++ src/api.js
@@ -1,12 +1,20 @@
 var m = require('mithril');
+var board = require('./board');
 
 module.exports = function(element, controller, view) {
 
   return {
     set: controller.set,
     toggleOrientation: controller.toggleOrientation,
-    getOrientation: controller.getOrientation,
-    getPieces: controller.getPieces,
+    getOrientation: function () {
+      return controller.data.orientation;
+    },
+    getPieces: function() {
+      return controller.data.pieces;
+    },
+    getMaterialDiff: function() {
+      return board.getMaterialDiff(controller.data);
+    },
     getFen: controller.getFen,
     dump: function() {
       return controller.data;
