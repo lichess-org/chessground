c1e4d9f392174e22478c7e4301488f8687992956
Thibault Duplessis <thibault.duplessis@gmail.com>
2019-07-13 22:49:32 +0200

    Revert "Change mousedown handling"
    This reverts commit 1de79bc4c028a1f34f74ff2939dc020892f90878.

--- src/events.ts
+++ src/events.ts
@@ -15,10 +15,10 @@ export function bindBoard(s: State): void {
   const boardEl = s.dom.elements.board,
   onStart = startDragOrDraw(s);
 
+  // must NOT be a passive event!
 
-  // touchstart can't be passive because we disable scroll.
-  boardEl.addEventListener('touchstart', onStart as EventListener, { passive: false });
-  boardEl.addEventListener('mousedown', onStart as EventListener, { passive: true });
+  boardEl.addEventListener('touchstart', onStart as EventListener);
+  boardEl.addEventListener('mousedown', onStart as EventListener);
 
   if (s.disableContextMenu || s.drawable.enabled) {
     boardEl.addEventListener('contextmenu', e => e.preventDefault());
