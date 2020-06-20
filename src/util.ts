56032a378c00f8c23e9b0c55e2ca682d9b7a56ab
Niklas Fiekas <niklas.fiekas@backscattering.de>
2020-06-20 19:01:50 +0200

    micro-optimize distanceSq (hot in sort)

--- src/util.ts
+++ src/util.ts
@@ -40,8 +40,11 @@ export function containsX<X>(xs: X[] | undefined, x: X): boolean {
   return xs !== undefined && xs.indexOf(x) !== -1;
 }
 
-export const distanceSq = (pos1: cg.Pos, pos2: cg.Pos): number =>
-  Math.pow(pos1[0] - pos2[0], 2) + Math.pow(pos1[1] - pos2[1], 2);
+export const distanceSq = (pos1: cg.Pos, pos2: cg.Pos): number => {
+  const dx = pos1[0] - pos2[0],
+  dy = pos1[1] - pos2[1];
+  return dx * dx + dy * dy;
+}
 
 export const samePiece = (p1: cg.Piece, p2: cg.Piece): boolean =>
   p1.role === p2.role && p1.color === p2.color;
