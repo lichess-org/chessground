9473c6a9201e9ba8436cf794aeea27a106fa6643
topce <slobodan.filipovic@gmail.com>
2020-11-29 15:17:50 +0100

    use File and Rank to compose Key type

--- src/types.ts
+++ src/types.ts
@@ -1,8 +1,8 @@
 export type Color = typeof colors[number];
 export type Role = 'king' | 'queen' | 'rook' | 'bishop' | 'knight' | 'pawn';
-export type Key = 'a0' | 'a1' | 'b1' | 'c1' | 'd1' | 'e1' | 'f1' | 'g1' | 'h1' | 'a2' | 'b2' | 'c2' | 'd2' | 'e2' | 'f2' | 'g2' | 'h2' | 'a3' | 'b3' | 'c3' | 'd3' | 'e3' | 'f3' | 'g3' | 'h3' | 'a4' | 'b4' | 'c4' | 'd4' | 'e4' | 'f4' | 'g4' | 'h4' | 'a5' | 'b5' | 'c5' | 'd5' | 'e5' | 'f5' | 'g5' | 'h5' | 'a6' | 'b6' | 'c6' | 'd6' | 'e6' | 'f6' | 'g6' | 'h6' | 'a7' | 'b7' | 'c7' | 'd7' | 'e7' | 'f7' | 'g7' | 'h7' | 'a8' | 'b8' | 'c8' | 'd8' | 'e8' | 'f8' | 'g8' | 'h8';
 export type File = typeof files[number];
 export type Rank = typeof ranks[number];
+export type Key = 'a0' | `${File | Rank}`;
 export type FEN = string;
 export type Pos = [number, number];
 export interface Piece {
