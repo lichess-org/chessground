fa20ba5e25716f4453fd56880a44431849580e24
Andy Pilate <cubox@cubox.dev>
2021-03-09 22:39:01 +0100

    Update yellow colour from gold to yellow

--- src/state.ts
+++ src/state.ts
@@ -169,7 +169,7 @@ export function defaults(): HeadlessState {
         green: { key: 'g', color: '#15781B', opacity: 1, lineWidth: 10 },
         red: { key: 'r', color: '#882020', opacity: 1, lineWidth: 10 },
         blue: { key: 'b', color: '#003088', opacity: 1, lineWidth: 10 },
-        yellow: { key: 'y', color: '#e68f00', opacity: 1, lineWidth: 10 },
+        yellow: { key: 'y', color: '#ffff00', opacity: 1, lineWidth: 10 },
         paleBlue: { key: 'pb', color: '#003088', opacity: 0.4, lineWidth: 15 },
         paleGreen: { key: 'pg', color: '#15781B', opacity: 0.4, lineWidth: 15 },
         paleRed: { key: 'pr', color: '#882020', opacity: 0.4, lineWidth: 15 },
