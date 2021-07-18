8770227d7f74cbca485213691f60f55b344dbfc4
Thibault Duplessis <t@lichess.org>
2021-07-18 16:26:07 +0200

    allow faster animations

--- src/config.ts
+++ src/config.ts
@@ -114,7 +114,7 @@ export function configure(state: HeadlessState, config: Config): void {
   if (state.selected) setSelected(state, state.selected);
 
   // no need for such short animations
-  if (!state.animation.duration || state.animation.duration < 100) state.animation.enabled = false;
+  if (!state.animation.duration || state.animation.duration < 70) state.animation.enabled = false;
 
   if (!state.movable.rookCastle && state.movable.dests) {
     const rank = state.movable.color === 'white' ? '1' : '8',
