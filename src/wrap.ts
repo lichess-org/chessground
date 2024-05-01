d2cbe1b233aa2faff7c3ba1414d6a4e5883d90c3
Thibault Duplessis <t@lichess.org>
2024-05-01 14:23:47 +0200

    remove 8 unnecessary css selectorss

--- src/wrap.ts
+++ src/wrap.ts
@@ -66,11 +66,12 @@ export function renderWrap(element: HTMLElement, s: HeadlessState): Elements {
     const ranksPositionClass = s.ranksPosition === 'left' ? ' left' : '';
 
     if (s.coordinatesOnSquares) {
+      const rankN: (i: number) => number = s.orientation === 'white' ? i => i + 1 : i => 8 - i;
       files.forEach((f, i) =>
         container.appendChild(
           renderCoords(
             ranks.map(r => f + r),
-            'squares rank' + (i + 1) + orientClass + ranksPositionClass,
+            'squares rank' + rankN(i) + orientClass + ranksPositionClass,
           ),
         ),
       );
