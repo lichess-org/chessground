395dfe8707e8fb0b58717d5cc092162603e331e4
Gilles Hoarau <unicolored@gmail.com>
2024-05-01 10:10:21 +0200

    Add condition to render coordinates on squares with new class 'squares'

--- src/wrap.ts
+++ src/wrap.ts
@@ -1,6 +1,6 @@
 import { HeadlessState } from './state.js';
 import { setVisible, createEl } from './util.js';
-import { colors, files, ranks, Elements, cases } from './types.js';
+import { colors, files, ranks, squares, Elements } from './types.js';
 import { createElement as createSVG, setAttributes, createDefs } from './svg.js';
 
 export function renderWrap(element: HTMLElement, s: HeadlessState): Elements {
@@ -62,73 +62,19 @@ export function renderWrap(element: HTMLElement, s: HeadlessState): Elements {
   }
 
   if (s.coordinates) {
-    const orientClass = s.orientation === 'black' ? ' black' : ' white';
-    // TODO: validate the change from: const ranksPositionClass = s.ranksPosition === 'left' ? ' left' : '';
-    const ranksPositionClass = s.ranksPosition === 'left' ? ' left' : s.ranksPosition === 'on-square' ? ' on-square' : '';
+    const orientClass = s.orientation === 'black' ? ' black' : '';
+    const ranksPositionClass = s.ranksPosition === 'left' ? ' left' : '';
 
-
-    // console.log('➡️ ranks', ranks)
-    // console.log('⬆️ files', files)
-    if (ranksPositionClass === ' on-square') {
-      // container.appendChild(renderCoords(ranks, 'ranks' + orientClass + ranksPositionClass));
-      // container.appendChild(renderCoords(files, 'files' + orientClass ));
-
-      // const reversedFiles = [...files];
-      // console.log(orientClass)
-      // if (orientClass !== '') {
-      //   reversedFiles.reverse();
-      // }
-      // console.log(files, reversedFiles)
-    // ranks.forEach((rank) => {
-    //   // console.log('rank', rank)
-    //   const currentFile = files[parseInt(rank)-1];
-    //   console.log(rank, currentFile)
-    //     const cases: string[] = [];
-    //   ranks.forEach((rank) => {
-    //     cases.push(currentFile + rank);
-    //   })
-    //   container.appendChild(renderCoords(cases, 'ranks file-'+ currentFile + orientClass + ranksPositionClass));
-    // })
-    // files.forEach((file) => {
-    //   // console.log('rank', rank)
-    //   const currentFile = file;
-    //   console.log(currentFile)
-    //     const thecases: string[] = [];
-    //   // ranks.forEach((rank) => {
-    //   //   cases.push(currentFile + rank);
-    //   // })
-    //   cases[file].forEach(thecase => {
-    //     thecases.push(thecase);
-    //   })
-    //   console.log('the classssss', 'ranks file-'+ currentFile + orientClass + ranksPositionClass);
-    //
-    //   // container.appendChild(renderCoords(files, 'files file-'+ currentFile + orientClass + ranksPositionClass));
-    // })
-
-
-      // Object.keys(cases).forEach(((fileId: string) => {
-      //   // console.log(cases);
-      //   // console.log(fileId);
-      //   console.log('s.orientation', s.orientation)
-      //   const file = cases[fileId];
-      //   console.log('render file:', file, 'squares  file-'+ fileId + orientClass + ranksPositionClass)
-      //   container.appendChild(renderCoords(s.orientation === 'black' ? file : file.reverse(), 'squares  file-'+ fileId + orientClass + ranksPositionClass));
-      //   container.appendChild(renderCoords(ranks, 'squares  file-'+ fileId + orientClass + ranksPositionClass));
-      // }))
-
-      console.log(orientClass, ranksPositionClass)
-      container.appendChild(renderCoords(cases.a, 'files on-square rank1' + orientClass + ranksPositionClass));
-      container.appendChild(renderCoords(cases.b, 'files on-square rank2' + orientClass + ranksPositionClass));
-      container.appendChild(renderCoords(cases.c, 'files on-square rank3' + orientClass + ranksPositionClass));
-      container.appendChild(renderCoords(cases.d, 'files on-square rank4' + orientClass + ranksPositionClass));
-      container.appendChild(renderCoords(cases.e, 'files on-square rank5' + orientClass + ranksPositionClass));
-      container.appendChild(renderCoords(cases.f, 'files on-square rank6' + orientClass + ranksPositionClass));
-      container.appendChild(renderCoords(cases.g, 'files on-square rank7' + orientClass + ranksPositionClass));
-      container.appendChild(renderCoords(cases.h, 'files on-square rank8' + orientClass + ranksPositionClass));
-
+    if (s.coordinatesOnSquares) {
+      let rank = 1;
+      Object.keys(squares).forEach((file) => {
+        const rankClass = ' rank' + rank;
+        container.appendChild(renderCoords(squares[file], 'squares' + rankClass + orientClass + ranksPositionClass));
+        rank++;
+      });
     } else {
       container.appendChild(renderCoords(ranks, 'ranks' + orientClass + ranksPositionClass));
-      container.appendChild(renderCoords(files, 'files' + orientClass ));
+      container.appendChild(renderCoords(files, 'files' + orientClass));
     }
   }
 
@@ -154,23 +100,9 @@ function renderCoords(elems: readonly string[], className: string): HTMLElement 
   const el = createEl('coords', className);
   let f: HTMLElement;
   for (const elem of elems) {
-    // console.log('elem', elem)
     f = createEl('coord');
     f.textContent = elem;
-    // console.log(f)
     el.appendChild(f);
   }
   return el;
 }
-// function renderCoordsOnSquare(elems: readonly string[], className: string): HTMLElement {
-//   const el = createEl('coords', className);
-//   let f: HTMLElement;
-//   for (const elem of elems) {
-//     console.log('elem', elem)
-//     f = createEl('coord');
-//     f.textContent = elem;
-//     console.log(f)
-//     el.appendChild(f);
-//   }
-//   return el;
-// }
